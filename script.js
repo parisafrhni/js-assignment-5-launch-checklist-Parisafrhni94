c55b5d16acdfbf863587de41fa1e16d1d374f773
parisafrhni <111383206+parisafrhni@users.noreply.github.com>
2023-07-28 18:52:38 -0400

    .

--- script.js
+++ script.js
@@ -1,16 +1,42 @@
 // Write your JavaScript code here!
+//script.js
+// Write your JavaScript code here!
+//script.js
+window.addEventListener("load", function () {
 
-window.addEventListener("load", function() {
+    let listedPlanets;
+    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
+    let listedPlanetsResponse = myFetch();
+    listedPlanetsResponse.then(function (result) {
+        listedPlanets = result;
+        console.log(listedPlanets);
+    }).then(function () {
+        console.log(listedPlanets);
+        // Below this comment call the appropriate helper functions to pick a planet from the list of planets and add that information to your destination.
+        let destination = pickPlanet(listedPlanets);
+        addDestinationInfo(
+            document,
+            destination.name,
+            destination.diameter,
+            destination.star,
+            destination.distance,
+            destination.moons,
+            destination.image
+        );
 
-   let listedPlanets;
-   // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-   let listedPlanetsResponse;
-   listedPlanetsResponse.then(function (result) {
-       listedPlanets = result;
-       console.log(listedPlanets);
-   }).then(function () {
-       console.log(listedPlanets);
-       // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
-   })
-   
+        // Get the form and add a submit event listener
+        let form = document.querySelector("form");
+        form.addEventListener("submit", function (event) {
+            event.preventDefault();
+
+            // Rest of the formSubmission code here...
+            let pilot = document.querySelector("input[name=pilotName]").value;
+            let copilot = document.querySelector("input[name=copilotName]").value;
+            let fuelLevel = document.querySelector("input[name=fuelLevel]").value;
+            let cargoLevel = document.querySelector("input[name=cargoMass]").value;
+
+            formSubmission(document, listedPlanets, pilot, copilot, fuelLevel, cargoLevel);
+        });
+    });
+
 });
